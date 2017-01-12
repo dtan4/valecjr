252cea3624c4826b14c03d87c4bc84387796578c
Daisuke Fujita <dtanshi45@gmail.com>
2017-01-12 16:20:30 +0900

    Create aws package

---
+++ aws/aws.go
@@ -0,0 +1,12 @@
+package aws
+
+var (
+	// AWSAccessKeyID represents AWS_ACCESS_KEY_ID
+	AWSAccessKeyID string
+	// AWSSecretAccessKey represents AWS_SECRET_ACCESS_KEY
+	AWSSecretAccessKey string
+	// AWSRegion represents AWS_REGION
+	AWSRegion string
+	// IAMRoleARN represents IAM Role ARN to use
+	IAMRoleARN string
+)
